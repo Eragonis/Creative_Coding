883bd3917814cc75a5a85bf48bdce9974734d7d4
Eragonis <150899018+Eragonis@users.noreply.github.com.>
2025-03-31 13:52:21 +0200

    square diagonal starts

---
+++ JS/sketch3.js
@@ -0,0 +1,17 @@
+function sketch3(p) {
+    p.setup = function () {
+        p.createCanvas(450, 450);
+        p.strokeWeight(2);
+        p.background(0)
+
+        
+        for(let y = 0; y < p.height; y += 55) {
+            for(let x = 0; x < p.width; x += 55) {
+                
+            };
+        };
+    };
+ 
+}
+
+new p5(sketch3);
